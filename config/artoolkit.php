c3e60b50bbf225f07deff9c958a8648e4174c74f
JapSeyz <Jesper@oestertoft.dk>
2017-01-07 10:26:12 +0100

    Updated config to use .env before default fallback

--- config/artoolkit.php
+++ config/artoolkit.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'level' => 2, // 0 = few, 4 = many
-    'leveli' => 1, // 0 = few, 3 = many
+    'level' => env('ARTOOLKIT_LEVEL', 2), // 0 = few, 4 = many
+    'leveli' => env('ARTOOLKIT_LEVELI', 1), // 0 = few, 3 = many
 
-    'max_dpi' => '180',
-    'default_dpi' => 40, // Used if the image has no DPI exif data
-    'min_dpi' => '20',
+    'max_dpi' => env('ARTOOLKIT_MAX_DPI', 72),
+    'default_dpi' => env('ARTOOLKIT_DEFAULT_DPI', 40), // Used if the image has no DPI exif data
+    'min_dpi' => env('ARTOOLKIT_MIN_DPI', 20),
 ];
